9613e5298b50a4c826dcd473158760133ad0416f
Enoriz <unconfigured@null.spigotmc.org>
2020-06-19 15:38:28 +0200

    COLOR PICKER, c'est trop bien!

--- src/imr4power/Main.java
+++ src/imr4power/Main.java
@@ -1,14 +1,43 @@
 package imr4power;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import javafx.event.*;
+import javafx.scene.text.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello World");
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        stage.setTitle("ColorPicker");
+        Scene scene = new Scene(new HBox(20), 400, 100);
+        HBox box = (HBox) scene.getRoot();
+        box.setPadding(new Insets(5, 5, 5, 5));
+
+        final ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setValue(Color.DARKBLUE);
+
+        final Text text = new Text("Try the color picker!");
+        text.setFont(Font.font ("Verdana", 20));
+        text.setFill(colorPicker.getValue());
+        
+        colorPicker.setOnAction(new EventHandler() {
+            public void handle(Event t) {
+                text.setFill(colorPicker.getValue());               
+            }
+        });
+ 
+        box.getChildren().addAll(colorPicker, text);
+ 
+        stage.setScene(scene);
+        stage.show();
     }
 
 
